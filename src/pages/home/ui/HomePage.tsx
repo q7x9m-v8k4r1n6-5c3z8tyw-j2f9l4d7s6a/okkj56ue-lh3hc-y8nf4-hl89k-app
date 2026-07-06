37661102b8e635906a8b6b2ea91d2fdc612575b2
nhatnews <nhattan123of@gmail.com>
2026-07-06 15:30:49 +0700

    chore: set up Feature-Sliced Design structure

---
+++ src/pages/home/ui/HomePage.tsx
@@ -0,0 +1,10 @@
+import './HomePage.css'
+
+export function HomePage() {
+  return (
+    <main className="home-page">
+      <h1>OVC Move</h1>
+      <p>Frontend foundation is ready.</p>
+    </main>
+  )
+}
